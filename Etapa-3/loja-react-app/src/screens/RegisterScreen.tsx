b3c41ce5796a339b0414aa1e6d2cd64035f2ddb8
MrMystin <francisco.rocha@aluno.ifsp.edu.br>
2025-10-15 09:21:33 -0300

    Tela de Cadastro

--- Etapa-3/loja-react-app/src/screens/RegisterScreen.tsx
+++ Etapa-3/loja-react-app/src/screens/RegisterScreen.tsx
@@ -1,11 +1,72 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, {useState} from "react";
+import { View, TextInput, Button, StyleSheet, Text, SafeAreaView } from "react-native";
+
+import { requestRegister } from "../services/authService";
 
 export default function RegisterScreen({ navigation}: any) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleRegister = async () => {
+    try {
+      const token = await requestRegister(name, email, password);
+      console.log('Cadastro ok')
+    } catch(err: any) {
+      setError(err)
+    }
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Página de Registro</Text>
-      <Button title="Voltar" onPress={() => navigation.goBack()} ></Button>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text>Nome:</Text>
+        <TextInput 
+            style={styles.input}
+            value={name}
+            onChangeText={setName}
+            autoCapitalize="none"
+        />
+        <Text>Email:</Text>
+        <TextInput 
+            style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+            autoCapitalize="none"
+        />
+        <Text>Senha:</Text>
+        <TextInput 
+            style={styles.input}
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+        />
+        { error ? 
+            <Text 
+                style={{ color: 'red'}}
+            >
+            {error}
+            </Text> :
+            null
+        }
+        <Button title="Cadastrar" onPress={handleRegister} />
+        <Button title="Já tem conta? Faça o login" onPress={ () => navigation.navigate('Login') }/>
+      </View>
+    </SafeAreaView>
+  );
 }
+  
+  const styles = StyleSheet.create({
+      container: {
+          flex: 1,
+          justifyContent: 'center',
+          padding: 20,
+      },
+      input: {
+          borderWidth: 1,
+          borderColor: '#ccc',
+          padding: 8,
+          marginBottom: 12,
+      }
+});
