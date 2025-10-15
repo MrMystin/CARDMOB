dd877ca494a10c803770afcadb3e25062efb0c98
MrMystin <francisco.rocha@aluno.ifsp.edu.br>
2025-10-15 10:25:09 -0300

    Tela básica do perfil

---
+++ Etapa-3/loja-react-app/src/services/profileService.ts
@@ -0,0 +1,17 @@
+import Constants from 'expo-constants';
+
+const { apiUrl } = Constants.expoConfig?.extra || {};
+
+export async function requestProfileById(id: number): Promise<[]> {
+  try {
+    const response = await fetch(`${apiUrl}/api/users/${id}`);
+    let data = await response.json();
+    if (data.image == null) {
+      data.image = `${apiUrl}/uploads/placeholder.png`;
+    }
+    return Promise.resolve(data);
+  } catch (error) {
+    console.error(error);
+    return Promise.reject(error);
+  }
+}
